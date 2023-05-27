6475652ffdfd96410a4573e2c9eb7ea556fb5544
Twan-Reijinga <twan.reijinga@coderclass.nl>
2023-05-27 21:33:53 +0200

    addanalog read test script for MCP3008

--- scripts/MCP3008.py
+++ scripts/MCP3008.py
@@ -1,20 +1,60 @@
-import busio
-import digitalio
-import board
-import adafruit_mcp3xxx.mcp3008 as MCP
-from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO as GPIO
+from time import sleep
 
-# create the spi bus
-spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+GPIO.setmode(GPIO.BCM)
 
-# create the cs (chip select)
-cs = digitalio.DigitalInOut(board.D5)
+# Define GPIO pins
+CLK = 12
+DOUT = 16
+DIN = 20
+CS = 21
 
-# create the mcp object
-mcp = MCP.MCP3008(spi, cs)
+# Set GPIO direction
+GPIO.setup(CLK, GPIO.OUT)
+GPIO.setup(DOUT, GPIO.IN)
+GPIO.setup(DIN, GPIO.OUT)
+GPIO.setup(CS, GPIO.OUT)
 
-# create an analog input channel on pin 0
-chan = AnalogIn(mcp, MCP.P0)
+def read_adc(channel):
+    # Send start bit
+    GPIO.output(CS, GPIO.HIGH)
+    GPIO.output(CLK, GPIO.LOW)
+    GPIO.output(CS, GPIO.LOW)
 
-print('Raw ADC Value: ', chan.value)
-print('ADC Voltage: ' + str(chan.voltage) + 'V')
+    # Send command (start bit, single-ended, channel selection)
+    command = channel
+    command |= 0x18  # 0x18: Start bit + single-ended bit
+    command <<= 3    # Move to the next bit position
+    for _ in range(5):
+        if command & 0x80:
+            GPIO.output(DIN, GPIO.HIGH)
+        else:
+            GPIO.output(DIN, GPIO.LOW)
+        command <<= 1
+        GPIO.output(CLK, GPIO.HIGH)
+        GPIO.output(CLK, GPIO.LOW)
+
+    # Read data
+    adc_value = 0
+    for _ in range(12):
+        GPIO.output(CLK, GPIO.HIGH)
+        GPIO.output(CLK, GPIO.LOW)
+        adc_value <<= 1
+        if GPIO.input(DOUT):
+            adc_value |= 0x01
+
+    # Set CS high to end conversion
+    GPIO.output(CS, GPIO.HIGH)
+
+    return adc_value
+
+# Usage example
+while True:
+    channel = 0  # Channel 0 on MCP3008
+    value = read_adc(channel)
+    print(f"ADC Value (Channel {channel}): {value}")
+    sleep(1)
+
+# Clean up GPIO
+GPIO.cleanup()
+
