2319be0bdf13c244dde47050eeedb3419f07628b
Twan-Reijinga <twan.reijinga@coderclass.nl>
2023-07-04 15:57:01 +0200

    update volume lines if volume is updated

--- scripts/main.py
+++ scripts/main.py
@@ -110,7 +110,9 @@ def change_volume(os, ssh, volumeChange, canvas, volumeText):
 
 	volume = get_volume(os, ssh)
 	volumeStep = int(volume/volumeChange)
-	change_volume_text(canvas, volumeText, volumeStep, int(1/volumeChange))
+	maxVolumeStep = int(1/volumeChange)
+	change_volume_text(canvas, volumeText, volumeStep, maxVolumeStep)
+	display_volume_lines(canvas, volumeStep, maxVolumeStep, 48.0, 196.0, 8.0, 52.0, 8.0)
 	
 	print(volumeStep)
 	if os == 'Linux':
